dfad5e0f8291c13c74624d36d4067c0fb7a6902c
Mano <Koutouzosm@gmail.com>
2019-05-26 14:39:00 -0400

    added app.js content to display to page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Wrapper from "./components/Wrapper"
 import Card from "./components/Card"
 import logos from './logo.json';
 import './App.css';
-import { isExpressionWrapper } from '@babel/types';
+
 
 
 class App extends React.Component {
@@ -15,24 +15,25 @@ class App extends React.Component {
 
 
 
-render() {
-  return (
-    <Wrapper>
-      <h1
-        className="title">Logo List</h1>
-      {
-        this.state.logoList.map(logo => {
-          return (
-            <Card
-              key={logo.id}
-
-            />
-          )
-        })
-      }
-    </Wrapper>
-  )
-}
+  render() {
+    return (
+      <Wrapper>
+        <h1
+          className="title">Logo List</h1>
+        {
+          this.state.logoList.map(logo => {
+            return (
+              <Card
+                key={logo.id}
+                name={logo.name}
+                image={logo.image}
+              />
+            )
+          })
+        }
+      </Wrapper>
+    )
+  }
 }
 
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,23 @@
-import React from "react"
+import React from "react";
+import "./style.css";
 
-function Card (props) {
+function Card(props) {
     return (
         <div className="card">
             <div className="img-container">
-                <img src={props.image} alt="Car Logo's"/>
+                <img src={props.image}
+                    alt={props.name}
+                />
             </div>
         </div>
     )
-}
+    // const shuffledlogosList = this.state.logosList.sort(() => 0.5 - Math.random());
+
+    // this.setState({
+    //   friendList: shuffledFriendList
+    // });
+};
+
 
 
 export default Card;
