13bf0455a12070d98168ddc39b67be7982d69f8f
Mano <Koutouzosm@gmail.com>
2019-05-30 09:16:28 -0400

    Final push before deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from "./components/Header"
+import Jumbotron from "./components/Jumbotron"
 import Wrapper from "./components/Wrapper"
 import Card from "./components/Card"
 import logos from './logo.json';
@@ -12,23 +12,34 @@ class App extends React.Component {
 
   state = {
     logoList: logos,
-    idArray: []
+    key: [],
+    score: 0,
+    highScore: 0
   }
 
 
-  incrementCount() {
-    this.setState((state) => {
-      return {count: state.count + 1}
+
+  handleHighScore = () => {
+    const {score, highScore} = this.state
+    if (score > highScore){
+      this.setState({
+        highScore: score
+      })
+    }
+  }
+
+  incrementCount = () => {
+    this.setState(() => {
+      return {
+        score: this.state.score + 1
+      }
     });
   }
-  
-  handleScore() {
-    this.incrementCount();
-  }
+
 
   shuffleLogos = () => {
     const shuffledLogoList = this.state.logoList.sort(() => 0.5 - Math.random());
-  
+
     this.setState({
       logoList: shuffledLogoList
     });
@@ -36,25 +47,37 @@ class App extends React.Component {
 
   handleClick = id => {
     console.log(id);
-    if ((this.state.idArray).match()) {
-      console.log("lose");
-    }
 
-    else {
-      let tempArray = this.state.idArray + id;
-
+    if (this.state.key.includes(id)) {
+      alert('You picked the same logo, start again!!')
+      // shuffle cards and reset score to 0
       this.setState({
-        idArray: tempArray
+        score: 0,
+        key: [],
       })
-    };
-  }
+      this.handleHighScore()
+    }
+    else {
+      const newKeys = [...this.state.key, id];
+      // shuffle cards and update score by one
+      this.setState({
+        key: newKeys
+      })
+      console.log("win")
+      this.incrementCount()
+    }
+    this.shuffleLogos()
+  };
 
   render() {
+    console.log(this.state)
     return (
       <React.Fragment>
-        <Header />
+        <Jumbotron
+        currentScore = {this.state.score}
+        highScore = {this.state.highScore}
+        />
         <Wrapper>
-          <h1 className="title">Logo List</h1>
           {
             this.state.logoList.map(logo => {
               return (
--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -4,16 +4,17 @@ import './style.css'
 
  
 
-function Header (props) {
+function Jumbotron (props) {
 
     const { currentScore, highScore } = props;
 return(
 <div className="jumbotron jumbotron-fluid text-center">
     <h2>Let's Test Your Memory!!</h2>
+    <small>Start by clicking an image, when the images shuffle choose wisely!! Choose the same image and lose, choose a different image they will shuffle and choose again. Hit 25 different images to WIN!!</small>
     <p>Your Current Score: {currentScore}</p>
     <p>Your Highest Score: {highScore}</p>
 </div>
 )};
 
 
-export default Header;
+export default Jumbotron;
